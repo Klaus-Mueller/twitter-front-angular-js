f25fa33f50b229d7ae38c55ebde361a936ac8309
Klaus Mueller <Klausmueller@outlook.com>
2024-06-27 20:07:44 -0300

    Fix Login credentials

--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -9,20 +9,22 @@
 
     function LoginController($http, $location) {
         var vm = this;
-        vm.username = '';
+        vm.email = '';
         vm.password = '';
         vm.error = '';
 
         vm.login = function() {
             $http.post('http://localhost:4567/login', {
-                username: vm.username,
+                email: vm.email,
                 password: vm.password
+            }, {
+                withCredentials: true
             }).then(function(response) {
                 // Handle successful login
                 $location.path('/home'); // Redirect to home or desired path
             }).catch(function(error) {
                 // Handle login error
-                vm.error = 'Invalid username or password';
+                vm.error = 'Invalid email or password';
             });
         };
     }
